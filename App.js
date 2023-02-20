3ba041250581b2e36ff20cdc0617eaf077b097f7
xenius <xeni0103@stud.kea.dk>
2023-02-20 10:34:18 +0100

    The first working commit

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import Home from './src/Home';
+import NoteAdd from './src/NoteAdd';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+import Header from './src/Header';
+import Detail from './src/Detail';
+import { Ionicons } from "@expo/vector-icons";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerTintColor: 'white',
+          headerBackImage: () => (
+            <Ionicons name="arrow-back" size={40} color="white" />
+          ),
+        }}
+      >
+        <Stack.Screen
+          component={Home}
+          name="Home"
+          options={{
+            headerTitle: () => <Header name="Notes"/>,
+            headerStyle: {
+              backgroundColor:"orange",
+              height:120,
+            }
+          }}
+        />
+           <Stack.Screen
+            component={NoteAdd}
+            name="NoteAdd"
+            options={{
+            headerTitle: () => <Header name="Add Notes"/>,
+              headerStyle: {
+              backgroundColor:"orange",
+              height:120,
+            }
+          }}
+        />
+        <Stack.Screen
+            component={Detail}
+            name="Detail"
+            options={{
+            headerTitle: () => <Header name="Edit Notes"/>,
+              headerStyle: {
+              backgroundColor:"orange",
+              height:120,
+            }
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
